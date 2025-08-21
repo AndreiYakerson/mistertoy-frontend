061f6c844865f021c3595d1f93f34d9a4aa23f7f
AndreiYakerson <tubikwheels@gmail.com>
2025-08-22 01:11:38 +0300

    Add ToyPreview cmp

---
+++ src/cmps/ToyPreview.jsx
@@ -0,0 +1,11 @@
+
+export function ToyPreview({ toy }) {
+    return (
+        <article className="toy-preview" key={toy._id}>
+            <h2>{toy.name}</h2>
+            <p>Price: ${toy.price}</p>
+            <img src={toy.imgUrl} alt="" />
+            {toy.inStock ? <p className="in-stock" style={{color: 'blue'}}>In Stock</p> : <p className="out-of-stock" style={{color: 'red'}}>Out of Stock</p>}
+        </article>
+    )
+}
