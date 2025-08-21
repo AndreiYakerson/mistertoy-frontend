195e3be198364d1a6aab6ce57f57c7d7db7ad2ae
AndreiYakerson <tubikwheels@gmail.com>
2025-08-22 01:12:20 +0300

    Add HomePage cmp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 
 export function HomePage() {
     return (
+        <section>
         <h1>Home</h1>
+        <img className='main-img' src="./img/toy1.png" alt="" />
+        </section>
     )
 }
