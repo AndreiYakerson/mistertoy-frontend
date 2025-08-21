72d0277ea7c332e829382d3d423347217a2f7d00
AndreiYakerson <tubikwheels@gmail.com>
2025-08-21 15:54:03 +0300

    Add basic Header cmp

---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+
+export function AppHeader() {
+
+
+    return (
+        <header className="app-header full main-layout">
+            <h1>Mister Toy</h1>
+            <nav className="app-nav">
+                <NavLink to="/" >Home</NavLink>
+                <NavLink to="/about" >About</NavLink>
+                <NavLink to="/toy" >Toys</NavLink>
+            </nav>
+        </header>
+    )
+}
