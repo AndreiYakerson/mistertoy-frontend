73b916b12af4a1b92814a137b74b1ba9829702f1
AndreiYakerson <tubikwheels@gmail.com>
2025-08-22 01:12:07 +0300

    Add basic ToyIndex cmp

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -1,6 +1,64 @@
+import { useEffect, useState } from "react";
+import { ToyList } from "../cmps/ToyList.jsx";
+import { toyService } from "../services/toy.service-local.js";
 
 export function ToyIndex() {
+
+    const [toys, setToys] = useState([]);
+
+    useEffect(() => {
+        loadToys();
+    }, []);
+
+    function loadToys() {
+        return toyService.query()
+            .then(toys => {
+                console.log('Toys loaded:', toys);
+                setToys(toys);
+            })
+            .catch(err => {
+                console.error('Error loading toys:', err);
+            });
+    }
+
+    function onAddToy() {
+        const toy = toyService.getRandomToy();
+
+        toyService.save(toy)
+            .then(savedToy => {
+                setToys(prevToys => [...prevToys, savedToy]);
+                console.log('Saved toy:', savedToy);
+            })
+            .catch(err => {
+                console.error('Can not saving toy:', err);
+            });
+    }
+
+    function onRemoveToy(toyId) {
+        return toyService.remove(toyId)
+            .then(() => setToys(prevToys => prevToys.filter(toy => toy._id !== toyId)))
+            .catch(err => {
+                console.error('Can not removing toy:', err);
+            });
+    }
+
+
+
     return (
-        <h1>Toys</h1>
+        <div className="toy-index">
+            <header>
+            </header>
+            <main>
+                <button onClick={onAddToy}>Add toy</button>
+            </main>
+
+            <section>
+                <hr />
+                <h1>Toy list:</h1>
+                <ToyList onRemoveToy={onRemoveToy} toys={toys} />
+            </section>
+
+        </div>
     )
+
 }
